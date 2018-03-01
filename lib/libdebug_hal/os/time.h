731a851ce999b22d8a26d5636ecfbb1176034941
Adrian Chadd <adrian@freebsd.org>
2018-03-01 14:08:36 -0800

    The latest macosx SDK introduces the posix clock_gettime() so this stuff isn't required.  Trouble is, I don't yet detect these features.
    So, add a comment so I indeed remember to!

--- lib/libdebug_hal/os/time.h
+++ lib/libdebug_hal/os/time.h
@@ -5,8 +5,11 @@
  * Since this library may end up being included by other things that
  * implement their own POSIX shims, let's avoid namespace clashes.
  */
-
 #ifdef __APPLE__
+/*
+ * XXX TODO: this needs to include the compat bits only for earlier
+ * versions of macosx!
+ */
 #include "os/apple/time.h"
 #else
 #include <sys/time.h>
